a3a0123dcd62a6de37d68ac2c9e419da4cfed555
Hugo Grillet <64898654+MallowBot@users.noreply.github.com>
2025-04-10 17:33:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,15 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+
+@app.route('/decrypt/<valeur>')
+def decrypt(valeur):
+    try:
+        valeur_bytes = valeur.encode()
+        valeur_decrypted = f.decrypt(valeur_bytes)
+        return valeur_decrypted.decode()
+    except:
+        return "Erreur : impossible de décrypter cette valeur."
+
 if __name__ == "__main__":
   app.run(debug=True)
